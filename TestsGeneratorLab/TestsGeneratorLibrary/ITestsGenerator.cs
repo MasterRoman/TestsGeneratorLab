566b8476c78d72ffbef98466bd01ae856d9f8a4e
Roman Kozko <romanka2001@mail.ru>
2021-11-21 15:29:01 +0300

    add main data stuctures and interfaces

---
+++ TestsGeneratorLab/TestsGeneratorLibrary/ITestsGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TestsGeneratorLibrary.CodeStrucures;
+
+namespace TestsGeneratorLibrary
+{
+    public interface ITestsGenerator
+    {
+        Dictionary<string, string> generateTests(CodeData data);
+    }
+
+    public interface IParser
+    {
+        CodeData parse(string code); 
+    }
+}
